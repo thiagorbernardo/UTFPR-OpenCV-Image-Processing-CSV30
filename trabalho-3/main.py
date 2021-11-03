706216680c1c36efbce838521f146a09efd73aad
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-11-02 21:11:42 -0300

    feat: mean and gaussian finished

--- trabalho-3/main.py
+++ trabalho-3/main.py
@@ -44,14 +44,15 @@ def blurMask(mask, sigma, occ):
     gaussians = []
     means = []
     st_sigma = sigma
+    k_size = 3
     for i in range(occ):
-        k_size = sigma + 1 if sigma % 2 == 0 else sigma
         gaussians.append(cv2.GaussianBlur(sm_img.copy(), (0, 0), st_sigma))
-        print(st_sigma)
-        
-        for j in range(5*occ):
-            means.append(cv2.blur(sm_img.copy(), (k_size, k_size)))
+
+        mean_img_aux = sm_img.copy()        
+        for j in range(5):
+            mean_img_aux = cv2.blur(mean_img_aux, (k_size, k_size))
             k_size += 2
+        means.append(mean_img_aux)
         st_sigma *= 2
 
     gaussian_mask = gaussians.pop(0)
@@ -64,7 +65,7 @@ def blurMask(mask, sigma, occ):
     for mean_img in means:
         mean_mask += mean_img
 
-    return [gaussian_mask, mean_mask]
+    return [gaussian_mask, mean_mask, gaussians[2]]
 
 def bloomImg(img, mask):
     return cv2.addWeighted(img, 0.8, mask, 0.2, 0.0)
@@ -79,27 +80,18 @@ def main():
     img = img.astype(np.float32) / 255
 
     # 1 - Mask
-
     mask = brightPass(img)
-    # mask_blur1 = gBlur(mask, 20)
-    # mask_blur2 = gBlur(mask, 10)
-    # mask_blur3 = gBlur(mask, 5)
-    # mask_blur4 = gBlur(mask, 2.5)
 
-    # mask_final = np.add(mask_blur1, mask_blur2, mask_blur3)
-    # mask_final = mask_blur1 + mask_blur2 + mask_blur3 + mask_blur4
-    [gaussian_mask, mean_mask] = blurMask(mask, 3, 3)
+    # 2 - Blur
+    [gaussian_mask, mean_mask, test] = blurMask(mask, 3, 4)
 
-    cv2.imwrite(f"001 - out-{INPUT_IMAGE}", mask * 255)
-    cv2.imwrite(f"002 - out-{INPUT_IMAGE}", resizeImg(gaussian_mask, 2) * 255)
-    cv2.imwrite(f"003 - out-{INPUT_IMAGE}", resizeImg(mean_mask, 2) * 255)
-    # cv2.imwrite(f"0099 - out-{INPUT_IMAGE}", bloomImg(img, resizeImg(mask_final, 2)) * 255)
+    cv2.imwrite(f"1Mascara - out-{INPUT_IMAGE}", mask * 255)
+    cv2.imwrite(f"1Mascara Gaussiana - out-{INPUT_IMAGE}", resizeImg(gaussian_mask, 2) * 255)
+    cv2.imwrite(f"1Mascara Media - out-{INPUT_IMAGE}", resizeImg(mean_mask, 2) * 255)
+    cv2.imwrite(f"1Teste - out-{INPUT_IMAGE}", resizeImg(test, 2) * 255)
+    cv2.imwrite(f"1Bloom Gaussiano - out-{INPUT_IMAGE}", bloomImg(img, resizeImg(gaussian_mask, 2)) * 255)
 
-    # Gaussian Blur
-    # img_gblur1 = gBlur(img, 20)
-
-    # img_gblur2 = gBlur(img, 10)
-    # out1 = cv2.resize(img_gblur2, (int (img.shape[1]), int (img.shape[0])))
+    cv2.imwrite(f"1Bloom Media - out-{INPUT_IMAGE}", bloomImg(img, resizeImg(mean_mask, 2)) * 255)
 
     # cv2.imwrite(f"01 - out-{INPUT_IMAGE}", img_gblur1 * 255)
     # cv2.imwrite(f"02 - out-{INPUT_IMAGE}", out1 * 255)
