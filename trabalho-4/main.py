27abfd7b28af1d21d268ab15288c0e876460972d
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-11-22 22:36:56 -0300

    feat: trabalho 4

--- trabalho-4/main.py
+++ trabalho-4/main.py
@@ -1,11 +1,7 @@
 # ===============================================================================
-# Exemplo: blur .
-# -------------------------------------------------------------------------------
-# Autor: Bogdan T. Nassu
-# Universidade Tecnológica Federal do Paraná
-# ===============================================================================
-# Trabalho 2
+# Trabalho 4
 # Autores: Thiago Ramos e João Lucas
+# Universidade Tecnológica Federal do Paraná
 # ===============================================================================
 
 import sys
@@ -13,57 +9,28 @@ import timeit
 import numpy as np
 import cv2
 from math import *
-import statistics as st
 
 sys.setrecursionlimit(3000)
 
 # ===============================================================================
 
-
-def colorfullImg(img):
-    qtd, labels = cv2.connectedComponents(1 - img, connectivity=8)
-    qtd = qtd - 1
-
-    unique, counts = np.unique(labels, return_counts=True)
-    counter = dict(zip(unique, counts))
-
-
-    # Map component labels to hue val
-    label_hue = np.uint8(179*labels/np.max(labels))
-    blank_ch = 255*np.ones_like(label_hue)
-    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-
-    # cvt to BGR for display
-    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-
-    # set bg label to black
-    labeled_img[label_hue==0] = 0
-
-    # cv2.imshow('labeled.png', labeled_img)
-
-# ===============================================================================
-
-def magic(img):
-    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(cv2.GaussianBlur(img, (0,0), 3), connectivity=4)
+def calculateComponents(img):
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=4)
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
     img2 = np.zeros((output.shape))
     img2 = img2.astype(np.uint8)
 
     for i in range(0, nb_components):
-        if sizes[i] >= 30 and sizes[i] <= 3000:
+        if sizes[i] >= 30:
             img2[output == i + 1] = 1
-
 
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img2, connectivity=4)
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
-    # print("std", np.std(sizes))
-    print("sizes", np.unique(sizes))
     median = np.median(sizes)
     print("median", np.median(sizes))
-    # error = np.std(sizes) / sqrt(nb_components)
-    # print("error", error)
+
     averageComponentSize = np.mean(sizes)
     print("mean", averageComponentSize)
 
@@ -71,20 +38,12 @@ def magic(img):
     for i in range(0, nb_components):
         if sizes[i] >= median:
             components += round(np.sum(sizes[i])/median)
-            # components += round(np.sum(sizes[i])/(averageComponentSize - 3*error))
         else:
             components += 1
 
     return [components, img2]
 
 # ===============================================================================
-
-def resizeImg(img, multiplier):
-    img = cv2.resize(
-        img, (int(img.shape[1] * multiplier), int(img.shape[0] * multiplier))
-    )
-    return img
-
 
 def otsuBinarization(img):
     """
@@ -97,40 +56,29 @@ def otsuBinarization(img):
 
     copy = img.copy()
 
-    copy = np.where(copy > 0.75, 1, 0)
-    copy = copy.astype(np.uint8)
+    copy = cv2.adaptiveThreshold(copy, 255.0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, -10)
 
-    # otsuImg = cv2.adaptiveThreshold(copy, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-    # threshold, copy = cv2.threshold(copy.astype(np.uint8), 0, 1, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    opening = cv2.morphologyEx(copy, cv2.MORPH_OPEN, (3, 3), iterations=3)
-    return opening
-
-    return copy
+    erode = cv2.morphologyEx(copy, cv2.MORPH_ERODE, (3, 3), iterations=2)
+    return erode
 
 
 def main():
-    # for INPUT_IMAGE in ["205"]:#, "82", "114", "150", "205"]:
+    # for INPUT_IMAGE in ["60"]:#, "82", "114", "150", "205"]:
     for INPUT_IMAGE in ["60", "82", "114", "150", "205"]:
         img = cv2.imread(f"{INPUT_IMAGE}.bmp", cv2.IMREAD_GRAYSCALE)
         if img is None:
             print("Erro abrindo a imagem.\n")
             sys.exit()
 
-        # img = img.reshape((img.shape[0], img.shape[1], img.shape[2]))
-        img = img.reshape((img.shape[0], img.shape[1], 1))
-        img = img.astype(np.float32) / 255
-
         # cv2.imwrite(f"1 - bin - out-{INPUT_IMAGE}", ( 255 - img ) * 255)
         # cv2.imwrite(f"2 - bin - out-{INPUT_IMAGE}", img * 255)
-        cv2.imwrite(f"out/gray-{INPUT_IMAGE}.bmp", img * 255)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-gray.bmp", img)
         imgBin = otsuBinarization(img)
-        cv2.imwrite(f"out/otsu-{INPUT_IMAGE}.bmp", imgBin * 255)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-otsu.bmp", imgBin)
 
-        [components, imgMagic] = magic(imgBin)
+        [components, imgMagic] = calculateComponents(imgBin)
         print(f"{INPUT_IMAGE} - {components}")
-        cv2.imwrite(f"out/magic-{INPUT_IMAGE}.bmp", imgMagic * 255)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-magic.bmp", imgMagic * 255)
 
         cv2.waitKey()
         cv2.destroyAllWindows()
