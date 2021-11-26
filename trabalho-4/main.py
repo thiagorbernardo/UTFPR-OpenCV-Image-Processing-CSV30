37ad85068ccee244896997b7c5aed21c20b4943b
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-11-26 20:38:18 -0300

    feat: test images

--- trabalho-4/main.py
+++ trabalho-4/main.py
@@ -29,10 +29,8 @@ def calculateComponents(img):
     sizes = stats[1:, -1]; nb_components = nb_components - 1
 
     median = np.median(sizes)
-    print("median", np.median(sizes))
 
     averageComponentSize = np.mean(sizes)
-    print("mean", averageComponentSize)
 
     components = 0
     for i in range(0, nb_components):
@@ -41,19 +39,11 @@ def calculateComponents(img):
         else:
             components += 1
 
-    return [components, img2]
+    return components
 
 # ===============================================================================
 
-def otsuBinarization(img):
-    """
-    1. Otsu's Binarization
-    2. Tirar blobs e ruídos, procurar menores e maiores componentes para remover pintando
-    3. Contagem componentes conectados e pixel de arroz
-    4. Total de pixeis de arroz / contagem de componentes conectados
-    5. Blob > Tamanho médio do arroz ? tamanho do blob / tamanho médio do arroz: 1, 2, 3 : 1
-    """
-
+def adaptiveBinarization(img):
     copy = img.copy()
 
     copy = cv2.adaptiveThreshold(copy, 255.0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, -10)
@@ -63,22 +53,19 @@ def otsuBinarization(img):
 
 
 def main():
-    # for INPUT_IMAGE in ["60"]:#, "82", "114", "150", "205"]:
     for INPUT_IMAGE in ["60", "82", "114", "150", "205"]:
         img = cv2.imread(f"{INPUT_IMAGE}.bmp", cv2.IMREAD_GRAYSCALE)
         if img is None:
             print("Erro abrindo a imagem.\n")
             sys.exit()
 
-        # cv2.imwrite(f"1 - bin - out-{INPUT_IMAGE}", ( 255 - img ) * 255)
-        # cv2.imwrite(f"2 - bin - out-{INPUT_IMAGE}", img * 255)
-        cv2.imwrite(f"out/{INPUT_IMAGE}-gray.bmp", img)
-        imgBin = otsuBinarization(img)
-        cv2.imwrite(f"out/{INPUT_IMAGE}-otsu.bmp", imgBin)
+        # Image binary
+        imgBin = adaptiveBinarization(img)
+        # cv2.imwrite(f"out-{INPUT_IMAGE}-binary.bmp", imgBin)
 
-        [components, imgMagic] = calculateComponents(imgBin)
-        print(f"{INPUT_IMAGE} - {components}")
-        cv2.imwrite(f"out/{INPUT_IMAGE}-magic.bmp", imgMagic * 255)
+        # Image components
+        components = calculateComponents(imgBin)
+        print(f"Image: {INPUT_IMAGE} - Result: {components}")
 
         cv2.waitKey()
         cv2.destroyAllWindows()
