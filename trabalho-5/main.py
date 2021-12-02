30110e15a689a9d152caa82abea4aa1723a94c91
João Lucas <jlmizug@hotmail.com>
2021-12-02 20:38:13 -0300

    3 mascara

--- trabalho-5/main.py
+++ trabalho-5/main.py
@@ -16,8 +16,8 @@ sys.setrecursionlimit(3000)
 
 def main():
     #for INPUT_IMAGE in range(0,1):
-    for INPUT_IMAGE in range(9,10):
-    # for INPUT_IMAGE in range(10,11):
+    # for INPUT_IMAGE in range(9,10):
+    for INPUT_IMAGE in range(10,11):
     # for INPUT_IMAGE in range(11,12):
     #for INPUT_IMAGE in range(12,13):
     # for INPUT_IMAGE in range(13,14):
@@ -42,44 +42,44 @@ def main():
         print('Lig')
         print(image_copy[0][0][2])
         
-        maskHue = img.copy()
-        maskSat = img.copy()
-        maskLig = img.copy()    
+        # maskHue = img.copy()
+        # maskSat = img.copy()
+        # maskLig = img.copy()    
         # mask = mask.reshape((img.shape[0], img.shape[1], 1))
-        # mask = np.ones(mask.shape[:2], dtype="uint8")
+        maskHue = np.ones(img.shape[:2], dtype="uint8") * 255
+        maskLig = np.ones(img.shape[:2], dtype="uint8") * 255
+        maskSat = np.ones(img.shape[:2], dtype="uint8") * 255
 
         test = []
         for y in range(image_copy.shape[0]):
             for x in range(image_copy.shape[1]):
                 hue = image_copy[y][x][0] - 60
-                # print(hue)
-                if(hue < 0):
-                    
-                    hue = (hue * -2) / 120
-                else:
-                    hue = hue / 120
-
-                maskHue[y][x] = hue * 255
-                # print(hue)
                 Lig = image_copy[y][x][1]
-                if(Lig >  100):
-                    
-                    maskLig[y][x] = 255
-                elif(Lig < 30):
-                    maskLig[y][x] = 0
-
-                else:
-                    maskLig[y][x][1] =  maskLig[y][x][1]
                 Sat = image_copy[y][x][2]
-                if(Sat >  79):
-                    
-                    maskSat[y][x] = 255
-                    
-                elif(Lig < 30):
-                    maskSat[y][x] = 0
+                if(Lig > 76 and Lig < 178):
+                    if(Sat >76):
+                        if(hue < 0 ):
+                            
+                            hue = (hue * -2) / 120
+                        else:
+                            hue = hue / 120
+
+                        maskHue[y][x] = hue * 255
+                        
+                        maskSat[y][x] = 255
+                    else:
+                        maskSat[y][x] =  Sat
+
 
                 else:
-                    maskSat[y][x][1] =  maskSat[y][x][1]
+                    # if(Lig >  100):
+                        
+                    #     maskLig[y][x] = 255
+                    # elif(Lig < 30):
+                    #     maskLig[y][x] = 0
+
+                    # else:
+                    maskLig[y][x] = Lig
                 test.append(Sat)
 
 
@@ -88,10 +88,13 @@ def main():
         # print(maskHue)
         # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
         # Vizualize the mask
-        cv2.imwrite(f"out/{INPUT_IMAGE}-maskHue.bmp", 255 - maskHue)
+        maskHue = 255 - maskHue
+        # maskSat = 255 - maskSat
+        # maskLig = 255 - maskLig
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskHue.bmp", maskHue)
         cv2.imwrite(f"out/{INPUT_IMAGE}-maskSat.bmp", maskSat)
         cv2.imwrite(f"out/{INPUT_IMAGE}-maskLig.bmp", maskLig)
-        cv2.imwrite(f"out/{INPUT_IMAGE}-maskFinal.bmp", maskLig*maskSat*maskHue)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskFinal.bmp", ((maskLig/255)*(maskSat/255)*(maskHue/255))*255)
         cv2.waitKey()
         cv2.destroyAllWindows()
 
