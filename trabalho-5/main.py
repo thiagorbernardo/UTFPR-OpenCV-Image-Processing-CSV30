c03724a23e4c27bb8f1c301869796823c2bec27c
João Lucas <jlmizug@hotmail.com>
2021-12-02 19:39:29 -0300

    João

--- trabalho-5/main.py
+++ trabalho-5/main.py
@@ -15,12 +15,13 @@ sys.setrecursionlimit(3000)
 
 
 def main():
-    for INPUT_IMAGE in range(0,1):
-    # for INPUT_IMAGE in range(9,10):
+    #for INPUT_IMAGE in range(0,1):
+    for INPUT_IMAGE in range(9,10):
     # for INPUT_IMAGE in range(10,11):
     # for INPUT_IMAGE in range(11,12):
-    # for INPUT_IMAGE in range(12,13):
+    #for INPUT_IMAGE in range(12,13):
     # for INPUT_IMAGE in range(13,14):
+    # for INPUT_IMAGE in range(14,15):
         img = cv2.imread(f"img/{INPUT_IMAGE}.bmp", cv2.IMREAD_COLOR)
         print(img)
         if img is None:
@@ -38,9 +39,12 @@ def main():
 
         # Change color to RGB (from BGR)
         image_copy = cv2.cvtColor(image_copy, cv2.COLOR_BGR2HLS)
-        print(image_copy[0][0][0])
+        print('Lig')
+        print(image_copy[0][0][2])
         
-        mask = img.copy()
+        maskHue = img.copy()
+        maskSat = img.copy()
+        maskLig = img.copy()    
         # mask = mask.reshape((img.shape[0], img.shape[1], 1))
         # mask = np.ones(mask.shape[:2], dtype="uint8")
 
@@ -55,18 +59,39 @@ def main():
                 else:
                     hue = hue / 120
 
-                mask[y][x] = hue * 255
+                maskHue[y][x] = hue * 255
                 # print(hue)
-                
-                test.append(hue)
+                Lig = image_copy[y][x][1]
+                if(Lig >  100):
+                    
+                    maskLig[y][x] = 255
+                elif(Lig < 30):
+                    maskLig[y][x] = 0
 
-        print(np.average(test))
-        # print(np.average(test) * 255)
-        print(mask)
+                else:
+                    maskLig[y][x][1] =  maskLig[y][x][1]
+                Sat = image_copy[y][x][2]
+                if(Sat >  79):
+                    
+                    maskSat[y][x] = 255
+                    
+                elif(Lig < 30):
+                    maskSat[y][x] = 0
+
+                else:
+                    maskSat[y][x][1] =  maskSat[y][x][1]
+                test.append(Sat)
+
+
+        # print(np.average(test))
+        print(np.max(test))
+        # print(maskHue)
         # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
         # Vizualize the mask
-        cv2.imwrite(f"out/{INPUT_IMAGE}-mask.bmp", mask)
-
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskHue.bmp", 255 - maskHue)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskSat.bmp", maskSat)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskLig.bmp", maskLig)
+        cv2.imwrite(f"out/{INPUT_IMAGE}-maskFinal.bmp", maskLig*maskSat*maskHue)
         cv2.waitKey()
         cv2.destroyAllWindows()
 
